bc30a3a6c9843152c650e2af0dc4f77672c93964
droghetti <arthik88@gmail.com>
2014-02-05 09:30:52 +0100

    model refactoring

--- analysis/models.py
+++ analysis/models.py
@@ -37,3 +37,25 @@ class ChiaveIndiciClimatici(models.Model):
     class Meta:
         managed = False
         db_table = 'chiave_indici_climatici'
+
+
+class DatiIndiciClimatici(models.Model):
+
+    gen = models.FloatField(blank=True, null=True)
+    feb = models.FloatField(blank=True, null=True)
+    mar = models.FloatField(blank=True, null=True)
+    apr = models.FloatField(blank=True, null=True)
+    mag = models.FloatField(blank=True, null=True)
+    giu = models.FloatField(blank=True, null=True)
+    lug = models.FloatField(blank=True, null=True)
+    ago = models.FloatField(blank=True, null=True)
+    sett = models.FloatField(blank=True, null=True)
+    ott = models.FloatField(blank=True, null=True)
+    nov = models.FloatField(blank=True, null=True)
+    dic = models.FloatField(blank=True, null=True)
+    annua = models.FloatField(blank=True, null=True)
+    inverno = models.FloatField(blank=True, null=True)
+    primavera = models.FloatField(blank=True, null=True)
+    estate = models.FloatField(blank=True, null=True)
+    autunno = models.FloatField(blank=True, null=True)
+    nota = models.CharField(max_length=250, blank=True)
