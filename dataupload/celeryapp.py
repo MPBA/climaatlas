572f5938572745889eb90e03be6b100d8b25afa1
Marco DallaG <marco.dallagiacoma@gmail.com>
2014-05-07 10:24:44 +0200

    moved geoatlas stuff in scripts dir

--- dataupload/celeryapp.py
+++ dataupload/celeryapp.py
@@ -4,17 +4,13 @@ from celery import Celery
 from django.conf import settings
 import os
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'climatlas.settings')
-
 app = Celery('dataupload',
              broker='amqp://',
              backend='amqp://',
-             include=['dataupload.tasks'])
+             include=['tasks'])
 
 # Optional configuration, see the application user guide.
-app.conf.update(
-    CELERY_TASK_RESULT_EXPIRES=3600,
-)
+app.config_from_object('celeryconfig')
 
 if __name__ == '__main__':
     app.start()
--- scripts/celery/tasks.py
+++ scripts/celery/tasks.py
@@ -1,29 +1,114 @@
 # coding=utf-8
 from __future__ import absolute_import
 import time
-
-from .celery import app
-from celery import group, chain, chord
-from celery.utils.log import get_task_logger
-from django.conf import settings
-
-from .mapgen.genday import Mapgen
-
+import os
+import sys
+import inspect
+import functools
+from datetime import datetime
 import psycopg2
 
-logger = get_task_logger(__name__)
+from celeryapp import app
+from celery import group, chain, chord
+from celery.utils.log import get_task_logger
 
-dj_db = settings.DATABASES['default']
-psycopg_conf = {
-    'database': dj_db['NAME'],
-    'user': dj_db['USER'],
-    'password': dj_db['PASSWORD'],
-    'host': dj_db['HOST'],
-    'port': dj_db['PORT']
-}
+from local_settings import psycopg_conf
+
+# import mapgen
+currentdir = os.path.dirname(
+    os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, os.path.join(parentdir, 'mapgen'))
+
+from genday import Mapgen
 
 
-@app.task
+def track_status(name):
+    def dat_actual_decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            # statuses:
+            # 0: never executed
+            # 1: executing
+            # 2: already executed
+            with psycopg2.connect(**psycopg_conf) as conn:
+                with conn.cursor() as cur:
+                    # Q: OMG n00b this is vulnerable to SQL injection
+                    # A: I know, but this won't be accessible by anyone. NP.
+                    cur.execute(
+                        'UPDATE task_progress SET {0}_start=%s, {0}_status=1 WHERE active'.format(
+                            name),
+                        (datetime.now(),)
+                    )
+            ret = True
+            time.sleep(5)
+            # ret = func(*args, **kwargs)
+
+            with psycopg2.connect(**psycopg_conf) as conn:
+                with conn.cursor() as cur:
+                    cur.execute(
+                        'UPDATE task_progress SET {0}_end=%s, {0}_status=2 WHERE active'.format(
+                            name),
+                        (datetime.now(),)
+                    )
+
+            return ret
+
+        return wrapper
+
+    return dat_actual_decorator
+
+
+@app.task(queue='geoatlas')
+def set_started(name):
+    with psycopg2.connect(**psycopg_conf) as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                'UPDATE task_progress SET {0}_start=%s, {0}_status=1 WHERE active'.format(
+                    name),
+                (datetime.now(),)
+            )
+
+
+@app.task(queue='geoatlas')
+def set_finished(name):
+    with psycopg2.connect(**psycopg_conf) as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                'UPDATE task_progress SET {0}_end=%s, {0}_status=2 WHERE active'.format(
+                    name),
+                (datetime.now(),)
+            )
+
+
+def track_status_group(name):
+    def dat_actual_decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            # statuses:
+            # 0: never executed
+            # 1: executing
+            # 2: already executed
+            return set_started.si(name) | set_finished.si(name)
+            tasks_group = func(*args, **kwargs)
+            return set_started.si(name) | chord(tasks_group,
+                                                set_finished.si(name))
+
+        return wrapper
+
+    return dat_actual_decorator
+
+
+@app.task(queue='geoatlas')
+def chord_finisher(*args, **kwargs):
+    """
+    Ugly hack borrowed from http://stackoverflow.com/questions/15123772/
+    """
+    return "OK"
+
+
+@app.task(queue='geoatlas')
+@track_status('step1')
 def step1(params):
     with psycopg2.connect(**psycopg_conf) as conn:
         with conn.cursor() as cur:
@@ -36,7 +121,8 @@ def step1(params):
             )
 
 
-@app.task
+@app.task(queue='geoatlas')
+@track_status('step2')
 def step2(params):
     with psycopg2.connect(**psycopg_conf) as conn:
         with conn.cursor() as cur:
@@ -49,12 +135,13 @@ def step2(params):
             )
 
 
-@app.task
+@app.task(queue='geoatlas')
 def step3():
     pass
 
 
-@app.task
+@app.task(queue='geoatlas')
+@track_status('step4')
 def step4(params):
     if params['periods']:
         with psycopg2.connect(**psycopg_conf) as conn:
@@ -67,7 +154,8 @@ def step4(params):
                 )
 
 
-@app.task
+@app.task(queue='geoatlas')
+@track_status('step5')
 def step5(params):
     with psycopg2.connect(**psycopg_conf) as conn:
         with conn.cursor() as cur:
@@ -81,7 +169,8 @@ def step5(params):
             )
 
 
-@app.task
+@app.task(queue='geoatlas')
+@track_status('step6')
 def step6(params):
     with psycopg2.connect(**psycopg_conf) as conn:
         with conn.cursor() as cur:
@@ -105,7 +194,8 @@ def step6(params):
             )
 
 
-@app.task
+@app.task(queue='geoatlas')
+@track_status('step7')
 def step7(params):
     if params['periods']:
         with psycopg2.connect(**psycopg_conf) as conn:
@@ -120,7 +210,7 @@ def step7(params):
                 )
 
 
-@app.task
+@app.task(queue='geoatlas')
 def _step8(mean_year, pstart, pend):
     with psycopg2.connect(**psycopg_conf) as conn:
         with conn.cursor() as cur:
@@ -129,13 +219,15 @@ def _step8(mean_year, pstart, pend):
                 .format(mean_year, pstart, pend)
             )
 
-@app.task
+
+@app.task(queue='geoatlas')
+@track_status_group('step8')
 def step8_group(params):
     tasks = []
 
     # todo: GESTIRE POOL SEPARATI PER EVITARE DI FARE ESPLODERE IL SERVER
     if params['periods']:
-        years = range(params['min_date'], params['max_date']+1)
+        years = range(params['min_date'], params['max_date'] + 1)
         periods = params['periods']
     else:
         years = params['years']
@@ -154,12 +246,13 @@ def step8_group(params):
     return group(*tasks)
 
 
-@app.task
+@app.task(queue='geoatlas')
 def step9():
     #
     pass
 
-@app.task
+
+@app.task(queue='geoatlas')
 def _step10(pstart, pend):
     with psycopg2.connect(**psycopg_conf) as conn:
         with conn.cursor() as cur:
@@ -169,7 +262,8 @@ def _step10(pstart, pend):
             )
 
 
-@app.task
+@app.task(queue='geoatlas')
+@track_status_group('step10')
 def step10_group(params):
     tasks = []
     if params['periods']:
@@ -180,7 +274,8 @@ def step10_group(params):
     return group(*tasks)
 
 
-@app.task
+@app.task(queue='geoatlas')
+@track_status('step11')
 def step11(params):
     with psycopg2.connect(**psycopg_conf) as conn:
         with conn.cursor() as cur:
@@ -192,13 +287,13 @@ def step11(params):
             )
 
 
-@app.task
+@app.task(queue='geoatlas')
 def _generate_day(day, type_prefix):
     m = Mapgen("/geostore/mapgen/bins", "/geostore/tiffs")
     m.generate_day(type_prefix, day.year, day.month, day.day)
 
 
-@app.task
+@app.task(queue='geoatlas')
 def generate_days_group(params):
     tasks = []
 
@@ -209,15 +304,28 @@ def generate_days_group(params):
     return group(*tasks)
 
 
-@app.task
+@app.task(queue='geoatlas')
 def generate_all(params):
     # va eseguito dopo avere modificato i giorni, non prende parametri
     m = Mapgen("/geostore/mapgen/bins", "/geostore/tiffs")
     m.generate_all()
 
+    # set active to False
+    with psycopg2.connect(**psycopg_conf) as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                'UPDATE task_progress SET active=false WHERE active=true')
 
-@app.task
+
+@app.task(queue='geoatlas')
 def launch_celery_tasks():
+    # fixme: are 3 different connections really necessary?
+    with psycopg2.connect(**psycopg_conf) as conn:
+        with conn.cursor() as cur:
+            cur.execute('SELECT COUNT(*) FROM task_progress WHERE active')
+            if cur.fetchone()[0]:
+                return
+
     with psycopg2.connect(**psycopg_conf) as conn:
         with conn.cursor() as cur:
             cur.execute("select * from import_all();")
@@ -231,7 +339,16 @@ def launch_celery_tasks():
     max_date = res[1] if res[1] > 2011 else 2011
     min_date, max_date = map(int, (min_date, max_date))
     dates = res[3]
-    years = list({ date.year for date in dates })
+
+    if dates is None:
+        return None
+
+    # DAT python2.6 which doesn't support set comprehension ~.~"
+    years = set()
+    for date in dates:
+        years.add(date.year)
+    years = list(years)
+
     p = dict(
         min_date=min_date,
         max_date=max_date,
@@ -239,23 +356,43 @@ def launch_celery_tasks():
         dates=dates,
         years=years
     )
+    with psycopg2.connect(**psycopg_conf) as conn:
+        with conn.cursor() as cur:
+            cur.execute('INSERT INTO task_progress(id) VALUES(DEFAULT)')
 
-    logger.info(u'==== LAUNCHING MEGACHAIN  ====')
-
-    (
+    return (
         step1.si(p) |  # launch step1 and wait until it finishes
-        group(  # then launch step2, 4, 8 and 11 together
-            (
-                group(step2.si(p), step4.si(p)) |
+        chord([  # then launch step2, 4, 8 and 11 together
+                 (
+                     chord([step2.si(p), step4.si(p)], chord_finisher.si()) |
 
-                # when both step2 and step4 have finished, launch step10,7,5,6.
-                # note that step10 spawns a bunch of subtasks
-                group(step10_group(p), step7.si(p), step5.si(p), step6.si(p))
-            ),
-            step8_group(p),
-            step11.si(p)
-        ) |  # when all of the above terminates, launch generate_days first
-        generate_days_group(p) |
-        generate_all.si(p)  # and generate_all then
-    )()
+                     # when both step2 and step4 have finished, launch step10,7,5,6.
+                     # note that step10 spawns a bunch of subtasks
+                     chord([step10_group(p), step7.si(p), step5.si(p),
+                            step6.si(p)], chord_finisher.si())
+                 ),
+                 step8_group(p),
+                 step11.si(p)
+              ], chord_finisher.si())  # |
+
+        # when all of the above terminates, launch generate_days first
+        #generate_days_group(p) |
+        #generate_all.si(p)  # and generate_all then
+    ).apply_async(queue='geoatlas')
+
+
+@app.task(queue='geoatlas')
+def generate_maps():
+    # todo: se chiamato solo per stazioni, settare tutti gli step a fatti e fare solo questo
+    with psycopg2.connect(**psycopg_conf) as conn:
+        with conn.cursor() as cur:
+            cur.call_proc('import_sites')
+            date_start, date_end = cur.fetchone()
+
+    if date_start and date_end:
+        m = Mapgen("/geostore/mapgen/bins", "/geostore/tiffs")
+        m.DATE_START = datetime.date(date_start.year, 1, 1)
+        m.DATE_END = datetime.date(date_end.year, 12, 31)
+        m.generate_all()
+
 
