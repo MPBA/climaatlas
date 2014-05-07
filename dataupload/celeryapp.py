350780aae208835b6df9a1f452c6d154d331c0f3
Marco DallaG <marco.dallagiacoma@gmail.com>
2014-05-07 10:27:02 +0200

    moved celery configuration to celeryconfig.py

--- dataupload/celeryapp.py
+++ dataupload/celeryapp.py
@@ -1,13 +1,8 @@
 from __future__ import absolute_import
 
 from celery import Celery
-from django.conf import settings
-import os
 
-app = Celery('dataupload',
-             broker='amqp://',
-             backend='amqp://',
-             include=['tasks'])
+app = Celery('dataupload')
 
 # Optional configuration, see the application user guide.
 app.config_from_object('celeryconfig')
